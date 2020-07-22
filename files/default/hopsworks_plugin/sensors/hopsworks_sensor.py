03201765fb2b25c8dc970e2eb3aeb887ae655836
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-07-22 16:30:13 +0200

    [HOPSWORKS-1920] Airflow Sensors pass API key to connection hook (#57)

--- files/default/hopsworks_plugin/sensors/hopsworks_sensor.py
+++ files/default/hopsworks_plugin/sensors/hopsworks_sensor.py
@@ -32,7 +32,30 @@ JOB_FINAL_APP_STATES = JOB_FAILED_FINAL_APP_STATES.union(JOB_SUCCESS_FINAL_APP_S
 
 JOB_FAILED_AM_STATUS = {'FAILED', 'KILLED'}
 
-class HopsworksJobFinishSensor(BaseSensorOperator):
+class HopsworksBaseSensor(BaseSensorOperator):
+    def __init__(
+            self,
+            hopsworks_conn_id = 'hopsworks_default',
+            job_name = None,
+            project_id = None,
+            project_name = None,
+            *args,
+            **kwargs):
+        super(HopsworksBaseSensor, self).__init__(*args, **kwargs)
+        self.hopsworks_conn_id = hopsworks_conn_id
+        self.job_name = job_name
+        self.project_id = project_id
+        self.project_name = project_name
+        if 'hw_api_key' in self.params:
+            self.hw_api_key = self.params['hw_api_key']
+        else:
+            self.hw_api_key = None
+
+    def _get_hook(self):
+        return HopsworksHook(self.hopsworks_conn_id, self.project_id, self.project_name, self.owner, self.hw_api_key)
+
+
+class HopsworksJobFinishSensor(HopsworksBaseSensor):
     """
     Sensor to wait for a job to finish regardless of the final state
 
@@ -56,15 +79,13 @@ class HopsworksJobFinishSensor(BaseSensorOperator):
             response_check = None,
             *args,
             **kwargs):
-        super(HopsworksJobFinishSensor, self).__init__(*args, **kwargs)
-        self.hopsworks_conn_id = hopsworks_conn_id
-        self.job_name = job_name
-        self.project_id = project_id
-        self.project_name = project_name
+        super(HopsworksJobFinishSensor, self).__init__(hopsworks_conn_id,
+                                                       job_name,
+                                                       project_id,
+                                                       project_name,
+                                                       *args,
+                                                       **kwargs)
         self.response_check = response_check
-
-    def _get_hook(self):
-        return HopsworksHook(self.hopsworks_conn_id, self.project_id, self.project_name, self.owner)
 
     def poke(self, context):
         hook = self._get_hook()
@@ -77,7 +98,7 @@ class HopsworksJobFinishSensor(BaseSensorOperator):
         return app_state.upper() in JOB_FINAL_APP_STATES
 
 
-class HopsworksJobSuccessSensor(BaseSensorOperator):
+class HopsworksJobSuccessSensor(HopsworksBaseSensor):
     """
     Sensor to wait for a successful completion of a job
     If the job fails, the sensor will fail
@@ -101,14 +122,12 @@ class HopsworksJobSuccessSensor(BaseSensorOperator):
             timeout = 3600,
             *args,
             **kwargs):
-        super(HopsworksJobSuccessSensor, self).__init__(*args, **kwargs)
-        self.hopsworks_conn_id = hopsworks_conn_id
-        self.job_name = job_name
-        self.project_id = project_id
-        self.project_name = project_name
-
-    def _get_hook(self):
-        return HopsworksHook(self.hopsworks_conn_id, self.project_id, self.project_name, self.owner)
+        super(HopsworksJobSuccessSensor, self).__init__(hopsworks_conn_id,
+                                                        job_name,
+                                                        project_id,
+                                                        project_name,
+                                                        *args,
+                                                        **kwargs)
 
     def poke(self, context):
         hook = self._get_hook()
